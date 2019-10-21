ff9bde1913c17e9669ee8ec29ee82f3e8b936d7a
Haydn <haydnhkim@gmail.com>
2019-10-21 14:38:22 +0900

    Modify teardown

--- scripts/utils/teardown.js
+++ scripts/utils/teardown.js
@@ -1,16 +1,5 @@
 const path = require('path');
-const execa = require('execa');
 const shell = require('shelljs');
-
-// yarn remove will throw an error if you re-delete an already deleted package.
-// Therefore, it is processed as a script that does not output log.
-(async () => {
-  try {
-    await execa('yarn', ['remove', 'next', 'react', 'react-dom']);
-  } catch (err) {
-    console.log(err);
-  }
-})();
 
 // remove generated files in dev/next-app
 const nextAppDir = path.resolve(__dirname, '../../dev/next-app');
