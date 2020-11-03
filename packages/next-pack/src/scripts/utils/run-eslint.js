f66bd3468437370841287c6cf0b612fb32ad9126
Haydn <haydnhkim@gmail.com>
2020-11-04 00:56:53 +0900

    Improve eslint messages

--- packages/next-pack/src/scripts/utils/run-eslint.js
+++ packages/next-pack/src/scripts/utils/run-eslint.js
@@ -7,7 +7,7 @@ const setTTY = () => {
   if (threads.workerData.isTTY) {
     for (const stream of ['stdout', 'stdin', 'stderr']) {
       if (process[stream].isTTY === undefined) {
-        process[stream].isTTY = true
+        process[stream].isTTY = true;
       }
     }
   }
@@ -29,8 +29,8 @@ const setTTY = () => {
   const userDirs = files
     ? [...new Set(files)]
     : ['src', 'pages', 'components', 'server']
-      .map(dir => path.resolve(targetDir, dir))
-      .filter(dir => fs.existsSync(dir));
+        .map((dir) => path.resolve(targetDir, dir))
+        .filter((dir) => fs.existsSync(dir));
   const userConfigFile = [
     '.eslintrc.js',
     '.eslintrc.yaml',
@@ -38,39 +38,39 @@ const setTTY = () => {
     '.eslintrc',
     '.eslintrc.json',
   ]
-    .map(file => path.resolve(targetDir, file))
-    .find(file => fs.existsSync(file));
+    .map((file) => path.resolve(targetDir, file))
+    .find((file) => fs.existsSync(file));
   const userEsLintConfig = userConfigFile && require(userConfigFile);
   const userEsLintConfigRules = (userEsLintConfig || {}).rules || {};
   const userEsLintConfigWithoutRules = userEsLintConfig && {
     ...userEsLintConfig,
-    rules: {}
+    rules: {},
   };
 
-  const nextEsLintConfig = require(
-    '../../../config/eslint.js'
-  );
+  const nextEsLintConfig = require('../../../config/eslint.js');
   const nextEsLintConfigWithoutRules = {
     ...nextEsLintConfig,
-    rules: {}
+    rules: {},
   };
-  const hasCustomConfig = !!userEsLintConfig &&
+  const hasCustomConfig =
+    !!userEsLintConfig &&
     !dequal(userEsLintConfigWithoutRules, nextEsLintConfigWithoutRules);
   const baseConfig = hasCustomConfig
     ? userEsLintConfig
     : {
-      ...nextEsLintConfig,
-      rules: {
-        ...nextEsLintConfig.rules,
-        ...userEsLintConfigRules,
-      }
-    };
-  const eslintUserDirs = files ||
+        ...nextEsLintConfig,
+        rules: {
+          ...nextEsLintConfig.rules,
+          ...userEsLintConfigRules,
+        },
+      };
+  const eslintUserDirs =
+    files ||
     userDirs
-      .map(dir => [
+      .map((dir) => [
         `${path.resolve(dir, '**/*.js')}`,
         `${path.resolve(dir, '**/*.ts')}`,
-        `${path.resolve(dir, '**/*.tsx')}`
+        `${path.resolve(dir, '**/*.tsx')}`,
       ])
       .reduce((a, b) => a.concat(b), []);
   const cli = new CLIEngine({
@@ -80,11 +80,11 @@ const setTTY = () => {
   });
 
   // eslint execution function
+  const errors = new Map();
   let lastLintRunTime;
-  let prevMessage;
-  const lint = path => {
-    // Return if rerun within 2 seconds
-    if (Date.now() < lastLintRunTime + 2000) return;
+  const lint = (path) => {
+    // Return if rerun within 0.3 seconds
+    if (Date.now() < lastLintRunTime + 300) return;
 
     lastLintRunTime = Date.now();
     let report;
@@ -97,18 +97,47 @@ const setTTY = () => {
       return;
     }
 
+    // Check if the problem is resolved
+    const resolvedFilePaths = new Set();
+    let isPrintedError = false;
+    for (let res of report.results) {
+      const { messages, filePath } = res;
+      const ruleIdValue = messages.map((n) => n.ruleId || 0).join('');
+
+      if (messages.length) {
+        if (errors.get(filePath) === ruleIdValue) isPrintedError = true;
+        errors.set(filePath, ruleIdValue);
+      } else {
+        if (errors.has(filePath)) resolvedFilePaths.add(filePath);
+        errors.delete(filePath);
+      }
+    }
+
+    // Print problem resolved message
+    const colors = {
+      reset: '\x1b[0m',
+      underline: '\x1b[4m',
+      green: '\x1b[32m',
+    };
+    for (let path of resolvedFilePaths.keys()) {
+      threads.parentPort.postMessage(`
+${colors.underline}${path}${colors.reset}
+${colors.green}✔︎ problem resolved!${colors.reset}
+`);
+    }
+
     // Do not output if there is no message
     if (
-      report.results.map(n => n.messages).reduce((a, b) => a.concat(b), [])
+      report.results.map((n) => n.messages).reduce((a, b) => a.concat(b), [])
         .length === 0
     )
       return;
 
     const message = formatter(report.results);
-    // ignore same messages
-    if (message === prevMessage) return;
 
-    prevMessage = message;
+    // ignore same messages
+    if (isPrintedError) return;
+
     threads.parentPort.postMessage(message);
   };
   setTimeout(() => {
