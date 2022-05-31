31618e17dc3e0431265cddbe5fe278d99c331d3c
Haydn <haydnhkim@gmail.com>
2022-06-01 00:31:56 +0900

    Remove unnecessary code

--- packages/next-pack/src/utils/copy-common-config.js
+++ packages/next-pack/src/utils/copy-common-config.js
@@ -7,36 +7,19 @@ const targetDir = workspaceRoot || projectDir;
 // Copy specific configuration files to the project root
 (() => {
   const packageDir = path.resolve(__dirname, '..', '..');
-  const userPackageCode = fs.readFileSync(
-    path.resolve(targetDir, 'package.json'),
-    'utf8'
-  );
-  const userPackageJson = userPackageCode ? JSON.parse(userPackageCode) : {};
-
   const targetFiles = [
     '.editorconfig',
     '.gitattributes',
-    '.eslintrc.js',
     '.eslintrc.cjs',
-    '.prettierrc.js',
     '.prettierrc.cjs',
   ]
     .filter((file) => {
       const filePath = path.resolve(packageDir, file);
       if (!fs.existsSync(filePath)) return false;
       const userFilePath = path.resolve(targetDir, file);
-      return ![
-        userFilePath,
-        userFilePath.replace(/\.cjs$/, '.js'),
-        userFilePath.replace(/\.js$/, '.cjs'),
-      ].some((filePath) => fs.existsSync(filePath));
+      return !fs.existsSync(userFilePath);
     })
-    .map((file) => {
-      const filePath = path.resolve(packageDir, file);
-      return userPackageJson.type !== 'module' && file.endsWith('.cjs')
-        ? filePath.replace(/\.cjs$/, '.js')
-        : filePath;
-    });
+    .map((file) => path.resolve(packageDir, file));
 
   if (targetFiles.length === 0) return;
 
